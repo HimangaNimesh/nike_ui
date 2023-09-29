cbd72edaa0611e4a1708b257b650b5e0218dd430
himanganimesh2 <himanganimesh2@gmail.com>
2023-09-29 23:51:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="x1:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+    <section className="padding">
+      Popular products
+    </section>
+    <section className="padding">
+      Super quality
+    </section>
+    <section className="padding-x py-10">
+      Services
+    </section>
+    <section className="padding">
+      Special offers
+    </section>
+    <section className="padding">
+      Customer reviews
+    </section>
+    <section className="padding">
+      Subscribe
+    </section>
+    <section className="padding">
+      Footer
+    </section>
+  </main>
+);
+
+export default App;
